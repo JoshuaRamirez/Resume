a035ff1657ab1fede9a8c712d10f3aa5fcc64913
Joshua Ramirez <joshua.ramirez@me.com>
2021-02-05 11:49:24 -0600

    Formatting

--- src/components/EditContactInformation.vue
+++ src/components/EditContactInformation.vue
@@ -4,10 +4,14 @@
       <h3 class="md-sub-title" style="flex: 1">Contact Information</h3>
     </md-toolbar>
     <md-card-content>
-      <md-field><label>Name</label><md-input v-model="Domain.Name" /></md-field>
-      <md-field
-        ><label>Phone</label><md-input v-model="Domain.Phone"
-      /></md-field>
+      <md-field>
+        <label>Name</label>
+        <md-input v-model="Domain.Name" />
+      </md-field>
+      <md-field>
+        <label>Phone</label>
+        <md-input v-model="Domain.Phone" />
+      </md-field>
     </md-card-content>
   </md-card>
 </template>
