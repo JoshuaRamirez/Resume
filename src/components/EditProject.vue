095d10b32517bb487e9e095868d6fbe3fb123af2
Joshua Ramirez <joshua.ramirez@me.com>
2021-02-08 13:31:26 -0600

    Created a template.

--- src/components/EditProject.vue
+++ src/components/EditProject.vue
@@ -1,7 +1,36 @@
 <template>
-  <div>
-    {{ JSON.stringify(Project) }}
-  </div>
+  <md-card>
+    <md-toolbar md-elevation="1">
+      <h3 class="md-sub-title" style="flex: 1">Project {{ Project.Id }}</h3>
+    </md-toolbar>
+    <transition name="fade">
+      <md-progress-bar
+        v-if="!Project"
+        class="progress-bar"
+        md-mode="indeterminate"
+      ></md-progress-bar>
+    </transition>
+    <md-card-content>
+      <div v-if="Project">
+        <md-field>
+          <label>Title</label>
+          <md-input v-model="Project.Title" />
+        </md-field>
+        <md-field>
+          <label>Company</label>
+          <md-input v-model="Project.Company" />
+        </md-field>
+        <md-field>
+          <label>Start Date</label>
+          <md-input v-model="Project.StartDate" />
+        </md-field>
+        <md-field>
+          <label>End Date</label>
+          <md-input v-model="Project.EndDate" />
+        </md-field>
+      </div>
+    </md-card-content>
+  </md-card>
 </template>
 
 <style lang="sass" scoped>
@@ -9,24 +38,25 @@
 
 <script>
 import Runtime from "../Domain/Runtime";
-const findProject = function(id) {
+const findProject = function (id) {
   const profile = Runtime.CachedProfile;
   const project = profile.Projects.FindById(id);
   return project;
-}
+};
 export default {
   data() {
-    const id = this.$route.params.id;
-    const project = findProject(id);
     return {
-      Project: project,
+      Project: null,
     };
+  },
+  async created() {
+    this.Profile = await Runtime.LoadProfile();
   },
   watch: {
     $route() {
       const id = this.$route.params.id;
       const project = findProject(id);
-      this.$data.Project = project;
+      this.Project = project;
     },
   },
 };
